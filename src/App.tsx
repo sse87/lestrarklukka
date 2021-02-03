26aefd3d26e657f9df9fab6da385c926210d48c5
Sigurður Snær Eiríksson <sse1987@gmail.com>
2021-02-03 23:19:51 +0000

    Added alarm by changing bg color and a snooze btn

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import { formatTime } from './misc/formatFunctions';
 
 const INITIAL_COUNT_DOWN_TIME = 900;
 
-const App = () => {
+enum alarmStates {
+  OFF,
+  ON,
+  SNOOZED,
+}
+
+const App: React.FC = () => {
   const [initialTimer, setInitialTimer] = useState(INITIAL_COUNT_DOWN_TIME);
   const [stopwatch, setStopwatch] = useState(0);
   const [timer, setTimer] = useState(INITIAL_COUNT_DOWN_TIME);
@@ -33,15 +39,40 @@ const App = () => {
     }
   }, []);
 
+  // Alarm effect
+  const [isAlarmOn, setIsAlarmOn] = useState(alarmStates.OFF);
+  useEffect(() => {
+    if (isAlarmOn === alarmStates.OFF && timer < 0) {
+      setIsAlarmOn(alarmStates.ON);
+    } else if (isAlarmOn !== alarmStates.OFF && timer > 0) {
+      setIsAlarmOn(alarmStates.OFF);
+    }
+  }, [isAlarmOn, timer]);
+
   return (
-    <div>
+    <div
+      className={`${styles.appWrapper} ${
+        isAlarmOn === alarmStates.ON ? styles.bgAlarm : ''
+      }`}
+    >
       <div className={styles.buttonNav}>
         <button
-          className={styles.button + (isStopwatchRunning && !isTimerRunning ? '' : ` ${styles.hidden}`)}
+          className={
+            styles.button +
+            (isStopwatchRunning && !isTimerRunning ? '' : ` ${styles.hidden}`)
+          }
           onClick={() => setIsStopwatchRunning((state) => !state)}
         >
           Taka stóra pásu
         </button>
+        {isAlarmOn === alarmStates.ON && (
+          <button
+            className={styles.button}
+            onClick={() => setIsAlarmOn(alarmStates.SNOOZED)}
+          >
+            Blunda
+          </button>
+        )}
       </div>
       <div className={styles.clock}>{formatTime(stopwatch)}</div>
       <div className={styles.clock}>{formatTime(timer)}</div>
